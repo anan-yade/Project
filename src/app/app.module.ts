e72d837a89e2398ab876ee1bdcea7d9dd8678686
OptimalSource <rajatakhare18@gmail.com>
2022-06-02 10:44:45 +0530

    initial setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { EditUserComponent } from './edit-user/edit-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    UserListComponent,
+    EditUserComponent
   ],
   imports: [
     BrowserModule,
